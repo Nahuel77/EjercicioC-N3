6eb948c898f169d1c3c53abf1449507eddb772aa
Nahuel <70348604+Nahuel77@users.noreply.github.com>
2025-03-23 14:29:55 -0300

    Update ejercicio_1.cpp

--- ejercicio_1.cpp
+++ ejercicio_1.cpp
@@ -7,13 +7,13 @@ int main() {
 
     std::cout << "Pirámide de Pascal generada automáticamente:\n";
 
-    for (int i = 0; i < filas; i++) {
-        for (int j = i; j > 0; j--) {
-            pascal[j] = pascal[j] + pascal[j - 1];
+    for (int i = 0; i < filas; i++) {               //0 1   2   3
+        for (int j = i; j > 0; j--) {               //  1   2-1 3-2
+            pascal[j] = pascal[j] + pascal[j - 1];  //  1   1-1 1-2-1..
         }
-
+    
         for (int j = 0; j <= i; j++) {
-            std::cout << pascal[j] << " ";
+            std::cout << pascal[j] << " ";          //1 1   1-1
         }
         std::cout << std::endl;
     }
